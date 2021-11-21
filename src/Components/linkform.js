dccbefe53822f8b1ce00cf20eee8bb4deb24052a
Josoxd01 <90148404+Josoxd01@users.noreply.github.com>
2021-11-21 14:15:14 -0500

    Firebase-Connection

---
+++ src/Components/linkform.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const linkform=()=>{
+    return <h1>linkform</h1>;
+}
+export default linkform;
---
+++ src/Components/links.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const links=()=>{
+    return <h1>links</h1>;
+}
+
+export default links;
