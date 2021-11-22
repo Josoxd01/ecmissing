ce536455d2af57676e2045611559cfab73f00746
Andy Orozco <91579298+andyorozco9719@users.noreply.github.com>
2021-11-21 23:22:52 -0500

    cambios minuto 43

--- src/Components/linkform.js
+++ src/Components/linkform.js
@@ -1,66 +1,51 @@
 import React, { useState } from "react";
 const linkform = (props) => {
 
-    const initialStateValues = {
+  const initialStateValues={
         url: "",
         name: "",
-        description: "",
-    };
-    const [values, setValues] = useState(initialStateValues);
+        description:"",
+  };
+    const [ values, setValues]= useState(initialStateValues);
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setValues({...values, [name]: value })
+    const handleInputChange=(e)=>{
+        const {name, value}=e.target;
+        setValues({...values, [name]:value})
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit=(e)=>{
         e.preventDefault();
-        console.log(values)
-        props.anadirOEditar();
+        props.anadirOEditarEnlace(values);
+        setValues({...initialStateValues})
     };
 
-    return ( <
-        form className = "card card-body"
-        onSubmit = { handleSubmit } >
-        <
-        div className = "form-group input-group" >
-        <
-        div className = "input-group-text bg-light" >
-        <
-        i className = "material-icons" > insert_link < /i> <
-        /div> <
-        input type = "text"
-        className = "form-control"
-        placeholder = "https://someurl.com"
-        name = "url"
-        onChange = { handleInputChange }
-        /> <
-        /div> <
-        div className = "form-group input-group" >
-        <
-        div className = "input-group-text bg-light" >
-        <
-        i className = "material-icons" > create < /i> <
-        /div> <
-        input type = "text"
-        className = "form-control"
-        name = "name"
-        placeholder = "Nombre del Sitio Web"
-        onChange = { handleInputChange }
-        /> <
-        /div> <
-        div className = "form-group" >
-        <
-        textarea name = "description"
-        rows = "3"
-        className = "form-control"
-        placeholder = "Escribir descripcion"
-        onChange = { handleInputChange } > < /textarea>
+    return ( 
+        <form className = "card card-body" onSubmit={handleSubmit} >
+        <div className="form-group input-group">
+            <div className="input-group-text bg-light">
+                <i className="material-icons">insert_link</i>
+            </div>
+            <input type="text" 
+            className="form-control" 
+            placeholder="https://someurl.com" 
+            name="url"
+            onChange={handleInputChange}
+            />
+        </div>
 
-        <
-        /div> <
-        button className = "btn btn-primary btn-block" > Guardar < /button> <
-        /form>
-    )
+        <div className="form-group input-group">
+            <div className="input-group-text bg-light">
+                <i className="material-icons">create</i>
+            </div>
+
+            <input type="text" className="form-control" name="name" placeholder="Nombre del Sitio Web" onChange={handleInputChange}/>
+        </div>
+        <div className="form-group">
+            <textarea name="description" rows="3" className="form-control" placeholder="Escribir descripcion" onChange={handleInputChange}></textarea>
+            
+        </div>
+        <button className="btn btn-primary btn-block">Guardar</button>
+        </form>
+    );
 };
 export default linkform;
--- src/Components/links.js
+++ src/Components/links.js
@@ -1,22 +1,20 @@
 import React from "react";
 import Linkform from "./linkform";
 
-import { db } from '../firebase'
+import {db} from '../firebase'
 
 
-const links = () => {
+const links=()=>{
 
-    const anadirOEditarEnlace = async(linkObject) => {
+    const anadirOEditarEnlace=async (linkObject)=>{
         await db.collection('links').doc().set(linkObject);
         console.log('Nueva Tarea Agregada')
     }
 
-    return <div >
-        <
-        Linkform anadirOEditarEnlace = { anadirOEditarEnlace }
-    /> <
-    h1 > links < /h1> <
-        /div>;
+    return <div>
+        <Linkform  anadirOEditarEnlace={anadirOEditarEnlace}/>
+        <h1>links</h1>
+    </div>;
 }
 
 export default links;
