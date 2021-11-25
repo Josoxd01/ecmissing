3faf2bc0fa96ad10007489030dc284170c60da38
Josoxd01 <90148404+Josoxd01@users.noreply.github.com>
2021-11-24 20:00:14 -0500

    Funcion-Eliminar-min1:03

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import React,{useEffect} from "react";
+import React from "react";
 import './App.css';
 import Links from "./Components/links";
-import {db} from './firebase'
 
 function App() {
   
-  const getDesaparecidos = async ()=>{
-    const querySnapShot= await db.collection('Desaparecidos').get();
-    querySnapShot.forEach(doc=>{
-      console.log(doc.data( ))
-    })
-   }
-  useEffect(()=>{
-    getDesaparecidos();
-},[]);
+  
   return (
     <div className="container p-4">
       <div className="row">
--- src/Components/links.js
+++ src/Components/links.js
@@ -1,21 +1,63 @@
-import React from "react";
+
+import React, {useEffect,useState} from "react";
 import Linkform from "./linkform";
 
 import {db} from '../firebase'
 
-const links=()=>{
+
+const Links=()=>{
+
+    const [Links,setLinks] = useState([]);
 
     const addoredit=async (linkObject)=>{
         await db.collection('Desaparecidos').doc().set(linkObject);
-        console.log('Nueva Tarea Agregada')
+        console.log('Nueva Tarea Agregada');
+    }
+
+    const Eliminar = async id =>{
+    if(window.confirm('Estas seguro de querer eliminar esta ficha')){
+        await db.collection('Desaparecidos').doc(id).delete();
+        console.log('ficha Eliminada')
+    }
     };
 
+    const getLinks = async ()=>{
+        db.collection('Desaparecidos').onSnapshot((querySnapshot)=>{
+            const docs = [];
+            querySnapshot.forEach((doc) => {
+                docs.push({...doc.data(),id:doc.id});
+            });
+            setLinks(docs);
+        });
+       
+    };
 
+    useEffect(()=>{
+     getLinks();
+      },[]);
 
     return <div>
-        <Linkform  addoredit={addoredit}/>
-        <h1>links</h1>
+        <div className="col-md-8 p-2" >
+            <Linkform  addoredit={addoredit}/>  
+        </div>
+        
+        <div className="col-md-8 p-2">
+            {Links.map(link => (
+                <div className ="card mb-2" key = {link.id}>
+                    <div className ="card-body">
+                    <div className ="d-flex justify-content-between">
+                        <i className = "material-icons text-danger" onClick= {() => Eliminar(link.id)}>close</i>
+                        <h4>{link.Nombre}</h4>
+                    </div>
+                      <p>{link.Edad}</p>
+                      <p>{link.Descripcion}</p>
+                      <p>{link.Telefono} </p>
+                    </div>
+                </div>
+            ))}
+        </div>
+
     </div>;
 }
 
-export default links;
+export default Links;
